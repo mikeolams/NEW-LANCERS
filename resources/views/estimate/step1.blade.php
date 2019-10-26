193c5019cc8a2392dc98a736759ac70b7228d4f4
Osueke Christian <osueke.christian@gmail.com>
2019-10-26 09:24:41 +0100

    Fixed logged user stage 1 styling

--- resources/views/estimate/step1.blade.php
+++ resources/views/estimate/step1.blade.php
@@ -2,34 +2,171 @@
 <!-- Select Project -->
 
 @section('styles')
-<link rel="stylesheet" href="{{asset('css/step1.css')}}"/>
+    <style>
+        * {
+            margin-right: 0px;
+            margin-left: 0px;
+            font-family: Ubuntu;
+        }
+
+
+        /*navbar css*/
+        #container {
+            display: grid;
+            grid-template-columns: 1fr 8fr 2fr;
+        }
+
+
+        #container div {
+            box-shadow: 0px 4px 2px rgba(0, 0, 0, 0.1);
+            outline: none;
+            height: 60px;
+
+        }
+
+        #container p {
+            justify-content: center;
+            margin-top: 15px;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 1.3em;
+            color: #323A43;
+        }
+
+
+        div>#ext {
+            background: rgba(207, 204, 204, 0.4);
+            font-size: 24px;
+            font-weight: bold;
+            justify-content: center;
+            border: none;
+            color: white;
+            width: 100%;
+            height: 60px;
+            outline: none;
+            /*added outline none*/
+        }
+
+        div>#ext:hover {
+            background: #0ABAB5;
+        }
+
+        div>.close {
+            outline: none;
+        }
+
+        .close {
+
+            color: #C4C4C4;
+            width: 100%;
+
+        }
+
+        .navM i {
+            padding-top: 15px;
+        }
+
+        /*end of nav bar*/
+
+        @media (max-width: 976px) {
+
+            div>#ext {
+                font-size: 15px;
+            }
+        }
+
+
+        @media (max-width: 992px) {
+
+
+            #container p {
+                font-size: 19.5px;
+                margin-top: 9px;
+            }
+
+            div>#ext {
+                font-size: 20px;
+            }
+        }
+
+
+
+        @media (max-width: 767px) {
+
+            #container p {
+                margin-top: 5px;
+           }
+            #container p {
+                font-size: 17.5px;
+                margin-top: 12px;
+            }
+
+            div>#ext {
+                font-size: 18px;
+            }
+
+
+        }
+
+
+        @media (max-width: 576px) {
+            #container p {
+                font-size: 16px;
+                margin-top: 12px;
+            }
+
+            div>#ext {
+                font-size: 15px;
+            }
+
+        }
+        .card:hover{border: 1px solid black;}
+    </style>
+
+<!-- <link rel="stylesheet" href="{{asset('css/step1.css')}}"/> -->
+
 @endsection
 
 
 @section('content')
+<div id="container">
+    <div>
+        <button class="close navM"><span>
+                <i class="fa fa-times"></i>
+            </span></button>
+    </div>
+    <div>
+        <p class="nav cEstimate" id="cre">Create Estimate</p>
+    </div>
+    <div>
+        <input class="disabled" id="ext" type="button" value="NEXT">
+    </div>    
+</div>
+
+
 <div class="contaner">
-
-<header style="border-bottom: 2px solid rgb(223, 223, 223);">
-        <nav style="display: flex; ">
-            <a href="#"  @click.prevent="$router.push('/estimate')" class="column-1">
-                <img
-                    src="https://res.cloudinary.com/mide358/image/upload/v1570621469/clear_24px_rasbwc.png"
-                    alt="navIcon"
-                    />
-            </a>
-
-            <div class="column-2">Create Estimate</div>
-            <a href="#" @click.prevent="next" id="upperNext" class="column-3 float-right" style="border: 1px solid gray;
-               background: #0ABAB5 !important;">NEXT</a>
-        </nav>
-    </header>
-
-
-    <h1 class="">What project are you estimating?</h1>
-    <h1 style="color:red;">@if(null !== session('error')) {{session('error')}} @endif</h1>
+    <div class="clearfix"></div>
+    <br/>  <br/>
+    <div class="row ml-auto box justify-content-center">
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <h2>What project are you estimating for?</h2>
+            <br>
+            <p style="color:red;">@if(null !== session('error')) {{session('error')}} @endif</p>
+        </div>
+    </div>
+    <!-- <h3 class="text-center">What project are you estimating?</h3> -->
+    @if(session()->has('message.alert'))
+    <div class="text-center">
+        <button class=" alert alert-{{ session('message.alert') }}"> 
+            {!! session('message.content') !!}
+        </button>
+    </div>
+    @endif
+    
     <form method="post" action="/estimate/create/step2">
         @csrf
-        <div class="row ml-auto mr-auto box justify-content-center">
+        <div class="row ml-auto box justify-content-center">
+        <!-- <div class="row ml-auto mr-auto box justify-content-center"> -->
             <div class="col-sm-4">
                 <div class="card">
                     <div class="card-body">
@@ -54,24 +191,74 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">A new project</h5>
-                        <p class="card-text">Create a new estimate and set up a new project based on the
+                        <p style="margin-bottom: 3em;" class="card-text">Create a new estimate and set up a new project based on the
                             information.
                         </p>
-                        <input type="text" name="new_project" id="name" placeholder="Project Name">
+                        <input type="text" class="form-control" name="new_project" id="name" placeholder="Project Name">           
                     </div>
                 </div>
             </div>
+            
         </div>
-        <div class="col-12">
-            <!-- @if(session('success'))<p><span class="alert alert-success">{{session('success')}}</span></p>
-            @elseif(session('error'))<p><span class="">{{session('error')}}</span></p> @endif -->
+        <div class="row ml-auto box justify-content-center mt-20" style="margin-top: 20px;">
+            <div class="col-sm-4">
+                <input class="disabled" id="ext" type="submit" value="NEXT">
+            </div>
         </div>
-        <button type="submit" id="formNext" class="btn dark">NEXT</button>
     </form>
 </div>
 
 @endsection
 
 @section('script')
+    
+<script>
+        function verifyPath() {
+        let a_next =  document.querySelector('.a-next');
+        let next = document.querySelector('.next');
+        let bt = document.getElementById('btne');
 
+
+        
+        if (createProject.value !== "" && createProject.value.length >= 4 ) {
+             a_next.style.background = '#0ABAB5';
+             next.style.background = '#0ABAB5';
+              bt.disabled = false;
+
+
+            document.querySelector('.a-next').classList.remove('disabled');
+            document.querySelector('.next').classList.remove('disabled');
+        } else {
+
+            //console.log('here works');
+            document.querySelector('.next').style.background = 'rgba(207, 204, 204, 0.4)';
+            document.querySelector('.next').classList.add('disabled');
+            document.querySelector('.a-next').style.background = 'rgba(207, 204, 204, 0.4)';
+            document.querySelector('.a-next').classList.add('disabled');
+             bt.disabled = true;
+              bt.preventDefault();
+            
+            
+             
+        }
+    }
+    
+    let createProject = document.getElementById('createProject');
+    window.onload=function(){
+         createProject.addEventListener('keyup', verifyPath);
+    }
+   
+
+     function manage(createProject) {
+        let bt = document.getElementById('btne');
+        if (createProject.value != '') {
+            bt.disabled = false;
+        }
+        else {
+            bt.disabled = true;
+             bt.preventDefault(); 
+        }
+    }    
+    </script>
 @endsection
+
