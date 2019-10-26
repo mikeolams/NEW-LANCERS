5d66dbcd365d9d26f2f465ee6dfcf017b72613f8
Osueke Christian <osueke.christian@gmail.com>
2019-10-26 04:01:50 +0100

    Cleaned master branch files

--- resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.auth')
-
-@section('main-content')
-<div class="container">
-    <button class='create-invoice' onClick="window.location.href='/estimate/create/step1'">Create Estimate</button>
-    <h3>Estimates helps you place value to your work and time!</h3>
-    <p>Create an estimate and easily convert it to an invoice and send to your client.</p>
-</div> 
-@endsection
