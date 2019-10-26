e9a75561ac7edb3ddda59ee5d1939a98ef68ae56
Osueke Christian <osueke.christian@gmail.com>
2019-10-26 05:14:23 +0100

    Reverted migration renaming actions

--- database/migrations/2019_10_08_171721_create_subscription_plans_table.php
+++ database/migrations/2019_10_08_171721_create_subscription_plans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubPlansTable extends Migration
+class CreateSubscriptionPlansTable extends Migration
 {
     /**
      * Run the migrations.
