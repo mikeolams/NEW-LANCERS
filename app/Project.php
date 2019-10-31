e048218fec65d799aa4b4cc4f1434666aeb9db27
GMike the GD <mikeolams@yahoo.com>
2019-10-31 01:54:43 +0100

    fix bug on project.php file

--- app/Project.php
+++ app/Project.php
@@ -13,45 +13,29 @@ class Project extends Model
         return $this->belongsTo(User::class, 'user_id')->with('estimate');
     }
     
-<<<<<<< HEAD
-   
-=======
     public function profile(){
         return $this->belongsTo(Profile::class, 'user_id');
     }
     
->>>>>>> e9373dbc6847e1ef2b6bcc1556faa5d54ac7b42b
      public function client(){
         return $this->belongsTo(Client::class, 'client_id');
     }
 
-<<<<<<< HEAD
-   
-
-=======
->>>>>>> e9373dbc6847e1ef2b6bcc1556faa5d54ac7b42b
     public function estimate()
     {
         return $this->belongsTo('App\Estimate');
     }
-<<<<<<< HEAD
-
-=======
     
->>>>>>> e9373dbc6847e1ef2b6bcc1556faa5d54ac7b42b
     public function tasks()
     {
         return $this->hasMany('App\Task');
     }
 
-<<<<<<< HEAD
-=======
     public function invoice()
     {
         return $this->hasOne('App\Invoice', 'id', 'invoice_id');
     }
 
->>>>>>> e9373dbc6847e1ef2b6bcc1556faa5d54ac7b42b
     public static function generateTrackingCode()
     {
         return "LNCR_PTRCK_".time(); 
